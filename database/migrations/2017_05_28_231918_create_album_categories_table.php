a1170b1d7e82f4a35523adf2e444c6ff62a04aa3
Cosmo <shooliss@gmail.com>
2017-05-28 23:20:45 +0800

    Added album categories table

--- database/migrations/2017_05_28_231918_create_album_categories_table.php
+++ database/migrations/2017_05_28_231918_create_album_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAlbumsTable extends Migration
+class CreateAlbumCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateAlbumsTable extends Migration
      */
     public function up()
     {
-        Schema::create('albums', function (Blueprint $table) {
+        Schema::create('album_categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('photo_url');
-            $table->text('description');
-            $table->integer('category');
             $table->nullableTimestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateAlbumsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('albums');
+        Schema::dropIfExists('album_categories');
     }
 }
