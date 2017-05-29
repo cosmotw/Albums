2af476e3bf2cb24ecce239a783c43d6b7c7e33e1
Cosmo <shooliss@gmail.com>
2017-05-29 17:49:49 +0800

    Added album factory model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,23 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->defineAs(App\Albums::class, 'newPhotos', function (Faker\Generator $faker) {
+    $photosUrl = [
+            'https://c1.staticflickr.com/9/8497/29112731450_0ec33575b1_k.jpg',
+            'https://c1.staticflickr.com/2/1626/24725726591_bce442f06c_o.jpg',
+            'https://c1.staticflickr.com/8/7352/16545287551_023fc394e4_o.jpg',
+            'https://c1.staticflickr.com/1/567/22255378448_52ed3b1976_o.jpg',
+            'https://c1.staticflickr.com/5/4080/4949399460_fdbe32496a_o.jpg',
+            'https://c1.staticflickr.com/9/8119/30039372472_08810a26ac_o.jpg',
+            'https://c1.staticflickr.com/6/5666/29927785443_8e0d366eef_o.jpg'
+    ];
+
+    return [
+        'title' => str_random(10),
+        'photo_url' => $photosUrl[array_rand($photosUrl)],
+        'description' => str_random(100),
+        'category_id' => 1,
+        'created_at' => $faker->dateTime($max = 'now')
+    ];
+});
--- database/seeds/AlbumsTableSeeder.php
+++ database/seeds/AlbumsTableSeeder.php
@@ -12,27 +12,6 @@ class AlbumsTableSeeder extends Seeder
      */
     public function run()
     {
-        $photosUrl = [
-            'https://c1.staticflickr.com/9/8497/29112731450_0ec33575b1_k.jpg',
-            'https://c1.staticflickr.com/2/1626/24725726591_bce442f06c_o.jpg',
-            'https://c1.staticflickr.com/8/7352/16545287551_023fc394e4_o.jpg',
-            'https://c1.staticflickr.com/1/567/22255378448_52ed3b1976_o.jpg',
-            'https://c1.staticflickr.com/5/4080/4949399460_fdbe32496a_o.jpg',
-            'https://c1.staticflickr.com/9/8119/30039372472_08810a26ac_o.jpg',
-            'https://c1.staticflickr.com/6/5666/29927785443_8e0d366eef_o.jpg'
-        ];
-
-        foreach($photosUrl as $url) {
-            $date = Carbon::create(2016, 1, 1, 0, 0, 0);
-
-            DB::table('albums')->insert([
-                'title' => str_random(10),
-                'photo_url' => $url,
-                'description' => str_random(100),
-                'category' => rand(1, 10),
-                'created_at' => $date->addWeeks(rand(1, 52))->addHours(rand(1, 24))->addMinutes(rand(1, 59))->addSeconds(rand(1, 59))->format('Y-m-d H:i:s'),
-                'updated_at' => $date->addWeeks(rand(1, 52))->addHours(rand(1, 24))->addMinutes(rand(1, 59))->addSeconds(rand(1, 59))->format('Y-m-d H:i:s')
-            ]);
-        }
+        factory(App\Albums::class, 'newPhotos', 20)->create();
     }
 }
